a7da9bc07e10115356d5ddeb005156ff0353bf16
Araya <aray2891@mail.ru>
2023-10-10 13:33:14 +0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-console.log(
-  "1. При нажатии на кнопки: Gardens, Lawn, Planting происходит смена фокуса на услугах в разделе service +50\n2.	Accordion в секции prices реализация 3-х выпадающих списков об услугах и ценах + 50\n3. В разделе contacts реализован select с выбором городов +25\nИТОГО: 125"
-);
-
 // Burger menu
 
 const menuShowHide = (event) => {
@@ -15,161 +11,163 @@ const menuShowHide = (event) => {
 
 document.querySelector("body").addEventListener("click", menuShowHide);
 
-// Blur service option
+const buttons = document.querySelectorAll(".btn");
 
-const serviceButtons = [
-  document.querySelectorAll("button")[1],
-  document.querySelectorAll("button")[2],
-  document.querySelectorAll("button")[3],
+const pictures = [
+  ...document.querySelectorAll(".img"),
+  ...document.querySelectorAll(".img_3"),
 ];
-const serviceItems = document.querySelectorAll(".service_item");
-let activeBtns = [];
 
-const btnActive = (event) => {
-  serviceButtons.forEach((element) => {
-    const isThisBtn = event.target === element ? true : false;
-    const btnActive = activeBtns.indexOf(element);
-
-    isThisBtn & (btnActive === -1) & (activeBtns.length < 2)
-      ? activeBtns.push(element)
-      : isThisBtn & (btnActive === -1)
-      ? activeBtns.push(element) & activeBtns.shift()
-      : isThisBtn & (btnActive === 0)
-      ? activeBtns.shift()
-      : isThisBtn
-      ? activeBtns.pop()
-      : null;
-  });
-
-  serviceButtons.forEach((element) =>
-    activeBtns.indexOf(element) !== -1
-      ? element.classList.add("button_active")
-      : element.classList.remove("button_active")
-  );
-
-  if (activeBtns.length === 0) {
-    serviceItems.forEach((element) =>
-      element.classList.remove("service_item_blur")
-    );
-  } else {
-    serviceButtons[0].classList.contains("button_active")
-      ? serviceItems[0].classList.remove("service_item_blur") &
-        serviceItems[4].classList.remove("service_item_blur")
-      : serviceItems[0].classList.add("service_item_blur") &
-        serviceItems[4].classList.add("service_item_blur");
-    serviceButtons[1].classList.contains("button_active")
-      ? serviceItems[2].classList.remove("service_item_blur")
-      : serviceItems[2].classList.add("service_item_blur");
-    serviceButtons[2].classList.contains("button_active")
-      ? serviceItems[1].classList.remove("service_item_blur") &
-        serviceItems[3].classList.remove("service_item_blur") &
-        serviceItems[5].classList.remove("service_item_blur")
-      : serviceItems[1].classList.add("service_item_blur") &
-        serviceItems[3].classList.add("service_item_blur") &
-        serviceItems[5].classList.add("service_item_blur");
+function numActiveButtons() {
+  let count = 1;
+  for (const button of buttons) {
+    if (button.classList.contains("button_active")) {
+      count++;
+    }
   }
-};
+  return count;
+}
 
-serviceButtons.forEach((element) =>
-  element.addEventListener("click", btnActive)
+buttons.forEach((el) =>
+  el.addEventListener("click", () => {
+    if (el.classList.contains("button_active")) {
+      el.classList.remove("button_active");
+      count = numActiveButtons();
+      if (count === 1) {
+        pictures.forEach((picture) => {
+          picture.style.filter = "none";
+        });
+      } else {
+        blurImg();
+      }
+    } else {
+      count = numActiveButtons();
+      if (count <= 2) {
+        el.classList.add("button_active");
+        blurImg();
+      } else {
+        clearActiveClasses();
+        pictures.forEach((el) => {
+          el.style.filter = "none";
+        });
+        count = 0;
+      }
+    }
+  })
 );
+
+function clearActiveClasses() {
+  buttons.forEach((button) => {
+    button.classList.remove("button_active");
+  });
+}
+
+function blurImg() {
+  let arr = [];
+  for (const button of buttons) {
+    if (button.classList.contains("button_active")) {
+      var classNameArray = button.className.split(" ");
+      console.log(classNameArray);
+      for (const picture of pictures) {
+        if (picture.classList.contains(classNameArray[2])) {
+          arr.push(picture);
+        }
+      }
+    }
+  }
+  pictures.forEach((picture) => {
+    picture.style.filter = "blur(4px)";
+  });
+  arr.forEach((el) => {
+    el.style.filter = "none";
+  });
+}
 
 // Accordion menu (price section)
+document.addEventListener("DOMContentLoaded", () => {
+  const accordions = document.querySelectorAll(".price_item");
+  //const priceButtons = document.querySelectorAll(".acc_btn");
 
-const priceItems = document.querySelectorAll(".price_item");
-const priceButtons = document.querySelectorAll(".acc_btn");
-const priceItemsShow = document.querySelectorAll(".price_item_2");
+  accordions.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      const self = e.currentTarget;
+      //const control = self.querySelector(".accordion__control");
+      const content = self.querySelector(".price_item_2");
 
-const accordionShow = (event) => {
-  priceButtons.forEach((element, i) => {
-    if (element === event.target) {
-      priceItems[i].classList.toggle("price_item_show");
-      priceButtons[i].classList.toggle("acc_btn_show");
-      priceItemsShow[i].classList.toggle("price_item_2_show");
-      let arr = [0, 1, 2];
-      arr.splice(i, 1);
-      arr.forEach((element) => {
-        priceItems[element].classList.remove("price_item_show");
-        priceButtons[element].classList.remove("acc_btn_show");
-        priceItemsShow[element].classList.remove("price_item_2_show");
-      });
-    }
-  });
-};
-
-priceButtons.forEach((element) =>
-  element.addEventListener("click", accordionShow)
-);
-
-// Contacts section
-
-const contactList = document.querySelectorAll(".contact_li");
-const dataText = document.querySelectorAll(".p8");
-const data = [
-  {
-    0: "Canandaigua, NY",
-    1: "+1 585 393 0001",
-    2: "151 Charlotte Street",
-  },
-  {
-    0: "New York City",
-    1: "+1 212 456 0002",
-    2: "9 East 91st Street",
-  },
-  {
-    0: "Yonkers, NY",
-    1: "+1 914 678 0003",
-    2: "511 Warburton Ave",
-  },
-  {
-    0: "Sherrill, NY",
-    1: "+1 315 908 0004",
-    2: "14 WEST Noyes BLVD",
-  },
-];
-const contactTable = document.querySelector(".contact_ul");
-const contactContainer = document.querySelector(".contacts_container");
-const contactObject = document.querySelector(".contacts_object");
-
-const contactShow = () => {
-  if (
-    contactTable.classList.contains("contact_ul_show") === false &&
-    contactContainer.classList.contains("contacts_container_show") === true
-  ) {
-    contactObject.classList.remove("contacts_object_show");
-    contactContainer.classList.remove("contacts_container_show");
-    document.querySelector(".container").classList.remove("container_show");
-    document.querySelector(".woman").classList.remove("woman_show");
-    document.getElementById("city").innerHTML = "City";
-    document.getElementById("city").classList.remove("city_show");
-  } else {
-    contactTable.classList.toggle("contact_ul_show");
-    contactContainer.classList.toggle("contacts_container_show");
-    document.querySelector(".container").classList.toggle("container_show");
-    document.querySelector(".woman").classList.toggle("woman_show");
-    document.querySelector(".img1").classList.toggle("img1_show");
-  }
-};
-
-const dataShow = (event) => {
-  contactList.forEach((element, i) => {
-    if (event.target === element) {
-      contactTable.classList.remove("contact_ul_show");
-      contactTable.classList.remove("contact_ul_show");
-      contactObject.classList.add("contacts_object_show");
-      document.querySelector(".img1").classList.remove("img1_show");
-      document.getElementById("city").innerHTML = element.textContent;
-      document.getElementById("city").classList.add("city_show");
-
-      dataText.forEach((element, index) => {
-        element.innerHTML = data[i][index];
-        if (index === 1) {
-          document.querySelector(".img2").href = "tel:" + data[i][index];
+      // Закрыть все открытые аккордеоны
+      const openAccordions = document.querySelectorAll(
+        ".price_item.price_item_show"
+      );
+      openAccordions.forEach((accordion) => {
+        if (accordion !== self) {
+          accordion.classList.remove("price_item_show");
+          //priceButtons.classList.remove("acc_btn_show");
+          //const control = accordion.querySelector(".accordion__control");
+          const content = accordion.querySelector(".price_item_2");
+          //   control.setAttribute("aria-expanded", false);
+          //   content.setAttribute("aria-hidden", true);
+          content.style.maxHeight = null;
         }
       });
-    }
-  });
-};
 
-document.querySelector(".img1").addEventListener("click", contactShow);
-contactList.forEach((element) => element.addEventListener("click", dataShow));
+      // Открыть/закрыть текущий аккордеон
+      self.classList.toggle("price_item_show");
+
+      if (self.classList.contains("price_item_show")) {
+        // control.setAttribute("aria-expanded", true);
+        // content.setAttribute("aria-hidden", false);
+
+        content.style.maxHeight = 108 + "px";
+      } else {
+        // control.setAttribute("aria-expanded", false);
+        // content.setAttribute("aria-hidden", true);
+        content.style.maxHeight = null;
+      }
+    });
+  });
+});
+
+/*Contact us*/
+document.addEventListener("DOMContentLoaded", () => {
+  const contactsBox = document.querySelector(".contacts_container");
+  const contacts_content = document.querySelector(".contact_ul");
+  const contacts_btn = document.querySelector(".img1");
+  const cities = document.querySelectorAll(".contact_li");
+  const contactsText = document.querySelector("#city");
+  const cityCard = document.querySelector(".contacts_object");
+  const cityInfo = document.querySelectorAll(".p8");
+  const callUsLink = document.querySelector(".img2");
+
+  const adresses = [
+    ["Canandaigua, NY", "+1	585	393 0001", "151 Charlotte Street"],
+    ["New York City", "+1	212	456 0002", "9 East 91st Street"],
+    ["Yonkers, NY", "+1	914	678 0003", "511 Warburton Ave"],
+    ["Sherrill, NY", "+1	315	908 0004", "14 WEST Noyes BLVD"],
+  ];
+
+  contactsBox.addEventListener("click", () => {
+    contacts_content.classList.toggle("contact_ul_show");
+    contacts_btn.classList.toggle("img1_show");
+    cityCard.style.scale = "0";
+  });
+
+  cities.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      const self = e.currentTarget;
+      contactsText.innerHTML = self.innerHTML;
+      contacts_content.classList.toggle("contact_ul_show");
+      contactsBox.classList.add("contacts_container_show");
+      cityCard.style.scale = "1";
+      for (let i = 0; i < adresses.length; i++) {
+        if (self.innerText === adresses[i][0]) {
+          if (callUsLink) {
+            callUsLink.href = "tel:" + adresses[i][1];
+          }
+          for (let j = 0; j < cityInfo.length; j++) {
+            cityInfo[j].innerText = adresses[i][j];
+          }
+        }
+      }
+    });
+  });
+});
